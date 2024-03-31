2cbc6906e7ebfff2ad21826627aebdd0fd86d019
Nikshit Bhateja <iosdevelopernikshit@gmail.com>
2024-03-31 09:18:02 +0530

    Initial commit

---
+++ ShoppingDemo/ShoppingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingDemoApp.swift
+//  ShoppingDemo
+//
+//  Created by Nikk Bhateja on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
